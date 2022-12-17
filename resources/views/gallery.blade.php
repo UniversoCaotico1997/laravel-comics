97f2286b5a8346d9e16b97d83f583a6e20f0da35
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-12-17 19:42:00 +0100

    Add partilas-banner end finish

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -4,17 +4,27 @@
 
 
 <div class="container">
-    <div class="row">
+    <div class="row row-cols-6">
         @foreach($comics as $comic)
         <div class="col">
             <img src="{{$comic['thumb']}}" alt="">
-            <h6 class="text-white">{{$comic['title']}}</h6>
+            <h6 class="text-white mt-3">{{$comic['title']}}</h6>
         </div>
         <!-- /.col -->
         @endforeach
     </div>
     <!-- /.row -->
+    <div class="load_more my-5">
+        <h5>load more</h5>
+    </div>
+    <!-- /.container -->
 </div>
 <!-- /.container -->
+
+
+<section class="banner">
+    @include('partials.banner')
+</section>
+<!-- /.banner -->
 
 @endsection
